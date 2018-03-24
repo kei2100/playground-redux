b1ea8112f36cc80a3f0504a132e9b068d125c637
kei2100 <kei.arima.2100@gmail.com>
2018-03-25 00:03:21 +0900

    create store and add some test scripts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import { render } from 'react-dom'
+import { createStore } from 'redux'
+import todoApp from './reducers'
 
-render(
-  'hello',
-  document.getElementById('root')
-);
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './actions'
+
+// The Store is the object that brings actions and reducers together.
+// The store has the following responsibilities:
+
+// - Holds application state;
+// - Allows access to state via getState();
+// - Allows state to be updated via dispatch(action);
+// - Registers listeners via subscribe(listener);
+// - Handles un-registering of listeners via the function returned by subscribe(listener)
+
+let store = createStore(todoApp);
+
+// Log the initial state
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
